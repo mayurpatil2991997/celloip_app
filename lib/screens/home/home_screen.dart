6d3a45a7ee683e27214e7498746f536cdb5a0155
mayur <mayurpatil2991997@gmail.com>
2022-12-19 23:02:42 +0530

    Added home screen with add required widgets

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:celloip_task/widgets/button/button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,220 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        width: width * 1,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            // stops: [0.1, 0.5, 0.7, 0.9],
+            colors: [
+              Color.fromRGBO(18, 170, 115, 1),
+              Color.fromRGBO(19, 91, 70, 0.81),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 80.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text(
+                "Smartr",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              const CircleAvatar(
+                // radius : 20.0,
+                backgroundImage: AssetImage(
+                  'assets/images/men.png',
+                ),
+                radius: 80,
+                backgroundColor: Colors.transparent,
+              ),
+              const SizedBox(
+                height: 18.0,
+              ),
+              const Text(
+                "Fresh look, same login.",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/arrow.png',
+                    width: 20.0,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "SmartRecruiters",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10.0,
+                          // fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      Text(
+                        "candidate portal",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10.0,
+                          // fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      Text(
+                        "is now Smartr.",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10.0,
+                          // fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/logout.png',
+                      width: 20.0,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(
+                      width: 12.0,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Enter the same login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10.0,
+                            // fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        Text(
+                          "info used for your",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10.0,
+                            // fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        Text(
+                          "SmartrProfile",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10.0,
+                            // fontWeight: FontWeight.w500
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 6.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/refresh.png',
+                      width: 20.0,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(
+                      width: 12.0,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "If login details were",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10.0,
+                            // fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        Text(
+                          "saved, you may",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10.0,
+                            // fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        Text(
+                          "need to re-save.",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10.0,
+                            // fontWeight: FontWeight.w500
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50.0,
+              ),
+              const Text(
+                "Why Smartr? Read here",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10.0,
+                  // fontWeight: FontWeight.w500
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+               ButtonWidget(
+                text: "GET STARTED",
+                onTap: () {},
+                colorContainer: const Color.fromRGBO(191, 219, 209, 1),
+                colorText: const Color.fromRGBO(19, 91, 70, 0.81),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/button/button_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final Function onTap;
+  final Color colorContainer;
+  final Color colorText;
+  const ButtonWidget({Key? key, required this.text, required this.onTap, required this.colorContainer, required this.colorText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+       padding: EdgeInsets.symmetric(horizontal: 60.0),
+      child: Container(
+        height: 40.0,
+        child: GestureDetector(
+          onTap: () {
+            onTap;
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              // border: Border.all(
+              //   color: Color(0xFFF05A22),
+              //   style: BorderStyle.solid,
+              //   width: 1.0,
+              // ),
+              color: colorContainer,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                      color: colorText,
+                      fontFamily: 'Montserrat',
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
