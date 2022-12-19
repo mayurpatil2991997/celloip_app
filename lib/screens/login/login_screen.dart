d93e5ae5d76dd8a30ece23237fbbecb7487434ee
mayur <mayurpatil2991997@gmail.com>
2022-12-20 01:05:36 +0530

    Added login screen ui

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:celloip_task/widgets/button/button_widget.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,15 +15,145 @@ class _LoginScreenState extends State<LoginScreen> {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Color.fromRGBO(191, 218, 208, 1),
-      body: Stack(
-        children: [
-          CustomPaint(
-            child: Container(
-              height: height * 0.5,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            CustomPaint(
+              child: Container(
+                height: height * 0.5,
+              ),
+              painter: CurvePainter(),
             ),
-            painter: CurvePainter(),
-          ),
-        ],
+            Column(
+              children:  [
+                const Padding(
+                  padding: EdgeInsets.only(top: 100.0),
+                  child: Center(
+                    child: Text(
+                      "Smartr",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 250.0,
+                ),
+                const Center(
+                  child: Text(
+                    "Let’s get you started",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        filled: true,
+                        hintStyle: TextStyle(color: Colors.grey[800]),
+                        hintText: "Enter Email",
+                        fillColor: Colors.white70),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        filled: true,
+                        hintStyle: TextStyle(color: Colors.grey[800]),
+                        hintText: "Enter Password",
+                        fillColor: Colors.white70),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/tc.png',
+                      width: 20.0,
+                      height: 10.0,
+                      color: Colors.black,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "By signing up, you will declare that you read and ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 8.0,
+                            // fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        Text(
+                          "understand the Smartr Services Agreement.",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 8.0,
+                            // fontWeight: FontWeight.w500
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 26.0,
+                ),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   const Text(
+                     "Already have an account?  ",
+                     style: TextStyle(
+                       color: Colors.black,
+                       fontSize: 10.0,
+                       // fontWeight: FontWeight.w500
+                     ),
+                   ),
+                   const Text(
+                     "Sign in",
+                     style: TextStyle(
+                       color: Colors.blue,
+                       fontSize: 10.0,
+                       // fontWeight: FontWeight.w500
+                     ),
+                   ),
+                 ],
+               ),
+                SizedBox(
+                  height: 14.0,
+                ),
+                ButtonWidget(
+                  text: "GET STARTED",
+                  onTap: () {},
+                  colorText: Color.fromRGBO(255, 255, 255, 1),
+                  colorContainer: Color.fromRGBO(19, 91, 70, 0.81),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -80,13 +211,9 @@ class CurvePainter extends CustomPainter {
     var path = Path();
 
     // path.moveTo(0, size.height * 0.25);
-    path.lineTo(0,size.height);
+    path.lineTo(0, size.height);
     path.quadraticBezierTo(
-      size.width * 0.5,
-      size.height - 100,
-      size.width,
-      size.height
-       );
+        size.width * 0.5, size.height - 100, size.width, size.height);
     path.lineTo(size.width, 0);
     // path.close();
     // path.lineTo(0, 0);
